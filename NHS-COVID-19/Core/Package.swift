11e36d576043c6dde055530f2c067bdab3716c18
Mo Ramezanpoor <me@mohsenr.com>
2022-05-06 15:12:20 +0100

    Fix open source SPM resolution issues.

--- NHS-COVID-19/Core/Package.swift
+++ NHS-COVID-19/Core/Package.swift
@@ -47,7 +47,7 @@ let package = Package(
         .package(name: "AppConfiguration", url: "https://github.com/nihp-public/covid-19-app-configuration-public.git", .branch("master")),
         .package(name: "ZIPFoundation", url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.11"),
         .package(name: "swift-log", url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
-        .package(name: "RiskScore", url: "https://github.com/nihp-public/riskscore-swift-public.git", .upToNextMajor(from: "3.2.2")),
+        .package(name: "RiskScore", url: "https://github.com/nihp-public/riskscore-swift-public.git", .upToNextMajor(from: "3.2.1")),
         .package(name: "Lokalise", url: "https://github.com/lokalise/lokalise-ios-framework", from: "0.9.12"),
     ],
     targets: [
